baf6d6aaf3524047e8dbbaa95f45b6eb3a35cf8a
Jorgen Evens <jorgen@evens.eu>
2025-03-13 21:23:14 +0100

    feat(modbus): extract value ranges

--- convert_modbus.py
+++ convert_modbus.py
@@ -6,6 +6,7 @@
 #
 # Distributed under terms of the MIT license.
 
+import re
 import json
 from openpyxl import Workbook
 from openpyxl.reader.excel import load_workbook
@@ -26,10 +27,14 @@ for row in ws.iter_rows(min_row=3):
     else:
         print(',')
 
+    range = [ float(v.strip()) for v in (row[5].value or '').split('to') if re.match(r'^[-+]?\d+(.\d+)?$', v.strip()) ]
+    if len(range) < 2: range = None
+
     print(json.dumps({
         'service': row[0].value.split('.')[-1],
         'name': row[1].value,
         'type': row[3].value,
+        'range': range,
         'scalefactor': row[4].value,
         'topic': row[6].value,
         'writable': row[7].value == 'yes',
