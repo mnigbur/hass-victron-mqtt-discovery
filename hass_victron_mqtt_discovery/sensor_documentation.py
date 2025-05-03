a5c84910bbd926adcd2b529f6d357ac210e4fb2b
Jorgen Evens <jorgen@evens.eu>
2025-05-04 00:43:26 +0200

    feat(state_class): attempt to identify state_class from sensor docs

--- hass_victron_mqtt_discovery/sensor_documentation.py
+++ hass_victron_mqtt_discovery/sensor_documentation.py
@@ -6,13 +6,8 @@ class SensorDocumentation(Cache):
 
     def __init__(self, sensor_documentation_path):
         self.unit_device_class_map = {}
+        self.unit_state_class_map = {}
         self.read_file(sensor_documentation_path)
-
-    def add_unit_device_class_mapping(self, unit, clazz):
-        if unit in self.unit_device_class_map:
-            return
-
-        self.unit_device_class_map[unit] = clazz
 
     def read_file(self, path):
         cache_id = self.file_cache_id(path)
@@ -20,10 +15,25 @@ class SensorDocumentation(Cache):
 
         self.read_device_classes(page, cache_id=cache_id)
 
+    def add_unit_device_class_mapping(self, unit, clazz):
+        if unit in self.unit_device_class_map:
+            return
+
+        self.unit_device_class_map[unit] = clazz
+
+    def add_unit_state_class_mapping(self, unit, state_class):
+        if unit in self.unit_state_class_map:
+            return
+
+        self.unit_state_class_map[unit] = state_class
+
     def read_device_classes(self, page, cache_id=None):
         if cache_id is not None and self.has_cache(cache_id):
-            self.unit_device_class_map = self.load_cache(cache_id)
-            return
+            cache_item = self.load_cache(cache_id)
+
+            if isinstance(cache_item, list):
+                self.unit_device_class_map, self.unit_state_class_map = cache_item
+                return
 
         state_table = pq(page('#available-device-classes').next_all('table')[0])
 
@@ -34,9 +44,19 @@ class SensorDocumentation(Cache):
 
             for u in units:
                 self.add_unit_device_class_mapping(u, clazz)
+                self.add_unit_state_class_mapping(u, 'total' if 'over time' in description else 'measurement')
 
         if cache_id is not None:
-            self.update_cache(cache_id, self.unit_device_class_map)
+            self.update_cache(cache_id, [
+                self.unit_device_class_map,
+                self.unit_state_class_map,
+            ])
+
+    def state_class_from_unit(self, unit):
+        if unit in self.unit_state_class_map:
+            return self.unit_state_class_map[unit]
+
+        return None
 
     def device_class_from_unit(self, unit):
         if unit in self.unit_device_class_map:
